218eb6b29400e7ff3e757dda8fdcd8fac1231c79
汥坳赵 <zhaobingwang@foxmail.com>
2023-01-10 22:31:10 +0800

    chore: remove useless code & import some package

--- naive-emr-web/vite.config.ts
+++ naive-emr-web/vite.config.ts
@@ -1,15 +1,36 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { TDesignResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    AutoImport({
+      resolvers: [
+        TDesignResolver({
+          library: 'vue-next',
+        }),
+      ],
+    }),
+    Components({
+      resolvers: [
+        TDesignResolver({
+          library: 'vue-next',
+        }),
+      ],
+    }),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
