7634aa1a4b3dd1b21de06f9dad12bdf6dfbba4f0
汥坳赵 <zhaobingwang@foxmail.com>
2023-06-28 23:27:43 +0800

    init: add some new project

---
+++ naive-emr-webapp-next/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </nav>
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ naive-emr-webapp-next/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+// import TheWelcome from '../components/TheWelcome.vue';
+</script>
+
+<template>
+  <main>
+    <h1>Home</h1>
+  </main>
+</template>
