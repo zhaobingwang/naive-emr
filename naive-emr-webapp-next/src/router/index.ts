9b528f60b2dc4decb0713c09a9428537588c35a0
汥坳赵 <zhaobingwang@foxmail.com>
2023-06-28 23:52:14 +0800

    feat: basic console home editor

--- naive-emr-webapp-next/src/router/index.ts
+++ naive-emr-webapp-next/src/router/index.ts
@@ -10,6 +10,11 @@ const router = createRouter({
       component: HomeView,
     },
     {
+      path: '/console',
+      name: 'console',
+      component: () => import('../views/ConsoleHomeView.vue'),
+    },
+    {
       path: '/about',
       name: 'about',
       // route level code-splitting
---
+++ naive-emr-webapp-next/src/views/ConsoleHomeView.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="console">
+    <h2>console</h2>
+    <div class="emr-editor"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Editor, RowFlex } from '@zhao-internal/emr-editor'
+import { onMounted, reactive } from 'vue'
+
+const config = reactive({
+  header: [
+    {
+      value: 'naive-emr-header',
+      rowFlex: RowFlex.CENTER,
+    },
+  ],
+  main: [
+    {
+      value: 'xxxxxxxxxxxxxxxxx',
+    },
+  ],
+  footer: [
+    {
+      value: 'naive-emr-footer',
+      size: 12,
+      rowFlex: RowFlex.CENTER,
+    },
+  ],
+})
+onMounted(() => {
+  const container: Element | null = document.querySelector('.emr-editor')
+  if (container === null) return
+  new Editor(container, config, {})
+})
+</script>
+<style lang="less" scoped>
+.console {
+}
+</style>
